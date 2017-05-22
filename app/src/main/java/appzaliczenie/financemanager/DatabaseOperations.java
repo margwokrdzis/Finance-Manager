25e4845600ee951b257e8e676f4a22f4c8f0d5c6
KrzysztofPytel <krzysztof_pytel@vp.pl>
2017-05-22 22:22:11 +0200

    add_loginFunction

---
+++ app/src/main/java/appzaliczenie/financemanager/DatabaseOperations.java
@@ -0,0 +1,12 @@
+package appzaliczenie.financemanager;
+
+/**
+ * Created by User on 22.05.2017.
+ */
+
+public interface DatabaseOperations {
+    String LOGIN = "login";
+    String LOGIN_SERVICE = "http://v-ie.uek.krakow.pl/~s174753/finance/login.php";
+    String SUCCESS = "success";
+    String FAILED = "fail";
+}
--- app/src/main/java/appzaliczenie/financemanager/LoginActivity.java
+++ app/src/main/java/appzaliczenie/financemanager/LoginActivity.java
@@ -4,13 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements DatabaseOperations {
+
+    EditText userNameET, passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userNameET = (EditText) findViewById(R.id.loginET);
+        passwordET = (EditText) findViewById(R.id.passwordET);
     }
 
     public void CreateAccount(View view) {
@@ -18,4 +24,13 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ClientListActivity.class);
         startActivity(intent);
     }
+
+    public void onLogin(View view){
+        String userName = userNameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        BackgroundWorker loginWorker = new BackgroundWorker(this);
+        loginWorker.execute(userName, password, LOGIN);
+    }
+
 }
