bcfae36f8830e7265a59d1fecf38382cb07d6c88
Marcin <marcingwozdz69@gmail.com>
2017-05-26 22:50:32 +0200

    Dodana funckja ogarniecznia wprowadznia kwot dla 6 liczb + 2liczby  po przecinku

--- app/src/main/java/appzaliczenie/financemanager/AddIncomeActivity.java
+++ app/src/main/java/appzaliczenie/financemanager/AddIncomeActivity.java
@@ -2,7 +2,11 @@ package appzaliczenie.financemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.method.DigitsKeyListener;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddIncomeActivity extends AppCompatActivity {
 
@@ -11,6 +15,39 @@ public class AddIncomeActivity extends AppCompatActivity {
         setTitle("Okno dodawania wpłat");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_income);
+
+        final EditText et = (EditText) findViewById(R.id.incomeAmountET);
+
+        et.setFilters(new InputFilter[] {
+                new DigitsKeyListener(Boolean.FALSE, Boolean.TRUE) {
+                    int beforeDecimal = 6, afterDecimal = 2;
+
+                    @Override
+                    public CharSequence filter(CharSequence source, int start, int end,
+                                               Spanned dest, int dstart, int dend) {
+                        String temp = et.getText() + source.toString();
+
+                        if (temp.equals(".")) {
+                            return "0.";
+                        }
+                        else if (temp.toString().indexOf(".") == -1) {
+                            // no decimal point placed yet
+                            if (temp.length() > beforeDecimal) {
+                                return "";
+                            }
+                        } else {
+                            temp = temp.substring(temp.indexOf(".") + 1);
+                            if (temp.length() > afterDecimal) {
+                                return "";
+                            }
+                        }
+
+                        return super.filter(source, start, end, dest, dstart, dend);
+                    }
+                }
+        });
+
+
     }
 
     public void addIncomeItem(View view){
--- app/src/main/java/appzaliczenie/financemanager/MyProfileActivity.java
+++ app/src/main/java/appzaliczenie/financemanager/MyProfileActivity.java
@@ -2,7 +2,11 @@ package appzaliczenie.financemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.method.DigitsKeyListener;
 import android.view.View;
+import android.widget.EditText;
 
 public class MyProfileActivity extends AppCompatActivity {
 
@@ -10,6 +14,9 @@ public class MyProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+
+
     }
 
     public void updateInfo(View view){
