5e14a65ca57dea1d24585373fbd34e5fa98336b5
Marcin <marcingwozdz69@gmail.com>
2017-05-26 22:38:35 +0200

    Zmiany podczas tworzenia konta, oraz uzupelniania danych firmy

---
+++ app/src/main/java/appzaliczenie/financemanager/MyProfileActivity.java
@@ -0,0 +1,18 @@
+package appzaliczenie.financemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+
+    public void updateInfo(View view){
+
+    }
+}
